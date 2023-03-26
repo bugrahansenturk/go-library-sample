580bec08af697b12439a8e59a877b284fabb2523
bugrahansntrk <bugrahansntrk@gmail.com>
2023-03-26 17:52:52 +0300

    add user

--- handlers/userHandler.go
+++ handlers/userHandler.go
@@ -1,25 +1,126 @@
 package handlers
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"strconv"
+	"strings"
+
+	domain "library-sample/domains"
+	"library-sample/router"
+	"library-sample/services"
 )
 
+var userService = services.NewUserService()
+
 func listUsers(w http.ResponseWriter, r *http.Request) {
-	// Handle listing all users
+	users := userService.ListUsers()
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(users)
 }
 
 func getUserByID(w http.ResponseWriter, r *http.Request) {
-	// Handle getting a user by ID
+	idStr := strings.TrimPrefix(r.URL.Path, "/users/get/")
+	id, err := strconv.Atoi(idStr)
+
+	if err != nil {
+		http.Error(w, "Invalid user ID", http.StatusBadRequest)
+		return
+	}
+
+	user, err := userService.GetUserByID(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
 }
 
 func addUser(w http.ResponseWriter, r *http.Request) {
-	// Handle adding a user
+	var user domain.User
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		http.Error(w, "Invalid JSON data", http.StatusBadRequest)
+		return
+	}
+
+	createdUser, err := userService.AddUser(user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(createdUser)
 }
 
 func updateUser(w http.ResponseWriter, r *http.Request) {
-	// Handle updating a user
+	idStr := strings.TrimPrefix(r.URL.Path, "/user/update/")
+	id, err := strconv.Atoi(idStr)
+
+	if err != nil {
+		http.Error(w, "Invalid user ID", http.StatusBadRequest)
+		return
+	}
+
+	var updatedUser domain.User
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	err = json.Unmarshal(body, &updatedUser)
+	if err != nil {
+		http.Error(w, "Invalid JSON data", http.StatusBadRequest)
+		return
+	}
+
+	updatedUser.ID = id
+	err = userService.UpdateUser(updatedUser)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
 
 func deleteUser(w http.ResponseWriter, r *http.Request) {
-	// Handle deleting a user
+	idStr := strings.TrimPrefix(r.URL.Path, "/user/delete/")
+	id, err := strconv.Atoi(idStr)
+
+	if err != nil {
+		http.Error(w, "Invalid user ID", http.StatusBadRequest)
+		return
+	}
+
+	err = userService.DeleteUser(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+
+}
+
+func UserRoutes() []router.Route {
+	return []router.Route{
+		{Path: "/user/list", Handler: listUsers},
+		{Path: "/user/get/", Handler: getUserByID},
+		{Path: "/user/add", Handler: addUser},
+		{Path: "/user/update/", Handler: updateUser},
+		{Path: "/user/delete/", Handler: deleteUser},
+	}
 }
